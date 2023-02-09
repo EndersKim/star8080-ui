572098fa8c856a2f7bf81920bd71c036c4ceeb5b
rocinate <rocinate1999@gmail.com>
2023-02-09 15:20:00 +0800

    add icon component

--- packages/components/components.d.ts
+++ packages/components/components.d.ts
@@ -1,6 +1,7 @@
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     starButton: typeof import('star8080-ui').Button
+    starIcon: typeof import('star8080-ui').Icon
   }
 }
 export {}
---
+++ packages/components/src/Icon/index.ts
@@ -0,0 +1,4 @@
+import icon from './icon.vue'
+import { withInstall } from '@star8080-ui/utils'
+const Icon = withInstall(icon)
+export default Icon
