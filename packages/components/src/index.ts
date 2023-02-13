b9596b26b8cf32f629941b29d872cc772d262659
enderskim <kinnnnng@qq.com>
2023-02-13 16:25:03 +0800

    add avatar

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,5 +1,6 @@
 import { default as Button } from './button'
 import { default as Icon } from './icon'
 import { default as Link } from './link'
-export { Button, Icon, Link }
-export default [Button, Icon, Link]
+import { default as Avatar } from './avatar'
+export { Button, Icon, Link, Avatar }
+export default [Button, Icon, Link, Avatar]
--- playground/app.vue
+++ playground/app.vue
@@ -1,8 +1,23 @@
 <template>
   <div>
     <Link href="https://juejin.cn/post/7121381989864701982">link</Link>
+
+    <h3>不同尺寸的头像</h3>
+    <Avatar
+      src="https://img.zhheo.com/i/2022/08/19/62ff32fa28da1.png"
+      size="small"
+    ></Avatar>
+    <Avatar src="https://img.zhheo.com/i/2022/08/19/62ff32fa28da1.png"></Avatar>
+    <Avatar
+      src="https://img.zhheo.com/i/2022/08/19/62ff32fa28da1.png"
+      size="large"
+    ></Avatar>
+    <h3>显示文字的头像</h3>
+    <Avatar name="俞涛"></Avatar>
+    <h3>默认头像</h3>
+    <Avatar></Avatar>
   </div>
 </template>
 <script lang="ts" setup>
-  import { Link } from 'star8080-ui'
+  import { Link, Avatar } from 'star8080-ui'
 </script>
