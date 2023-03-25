7a5d68cf61851e70ca9b6d7e9e68ea9c8155fa76
gitguoxiangyu <3317755916@qq.com>
2023-03-25 15:04:44 +0800

    test component in app

--- playground/app.vue
+++ playground/app.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable prettier/prettier -->
 <template>
   <div>
     <Link href="https://juejin.cn/post/7121381989864701982">link</Link>
@@ -27,31 +28,31 @@
     <h3>文字提示</h3>
 
     <Tooltip tip="文字提示">
-      <div style="padding: 10px; background-color: #ddd">把鼠标放在我上面</div>
+      <div style="padding: 10px; background-color: #ddd;">把鼠标放在我上面</div>
     </Tooltip>
 
     <h3>徽标</h3>
 
     <Badge>
-      <div style="width: 60px; height: 60px; background-color: #aaa"></div>
+      <div style="width: 60px; height: 60px; background-color: #aaa;"></div>
     </Badge>
 
     <h3>带有数字的徽标</h3>
 
     <Badge :num="20">
-      <div style="width: 60px; height: 60px; background-color: #aaa"></div>
+      <div style="width: 60px; height: 60px; background-color: #aaa;"></div>
     </Badge>
 
     <h3>带有涟漪效果的徽标</h3>
 
     <Badge ripple>
-      <div style="width: 60px; height: 60px; background-color: #aaa"></div>
+      <div style="width: 60px; height: 60px; background-color: #aaa;"></div>
     </Badge>
 
     <br />
 
     <Badge :num="2" ripple>
-      <div style="width: 60px; height: 60px; background-color: #aaa"></div>
+      <div style="width: 60px; height: 60px; background-color: #aaa;"></div>
     </Badge>
 
     <h3>不同尺寸的头像</h3>
@@ -70,23 +71,63 @@
     <Avatar></Avatar>
 
     <br />
-    <Switch v-model="test" mimicry />
+
+    <Scrollbar :height="400">
+      <ul>
+        <li>666</li>
+        <li>666</li>
+        <li>666</li>
+        <li>666</li>
+        <li>666</li>
+        <li>666</li>
+        <li>666</li>
+        <li>666</li>
+        <li>666</li>
+        <li>666</li>
+        <li>888</li>
+        <li>666</li>
+        <li>666</li>
+        <li>666</li>
+        <li>666</li>
+        <li>666</li>
+        <li>666</li>
+        <li>666</li>
+        <li>777</li>
+        <li>666</li>
+        <li>666</li>
+        <li>666</li>
+      </ul>
+    </Scrollbar>
+
+    <br />
+    <!-- <Input></Input> -->
+    <!-- <Switch v-model="test" mimicry /> -->
     <Waterfall value="star"></Waterfall>
+    <!-- <Select></Select> -->
+
+    <Casader :option="options" v-model="selectedValue"></Casader>
   </div>
+  <!-- <testtest></testtest> -->
 </template>
 <script lang="ts" setup>
+  import { log } from 'console'
   import {
     Link,
+    Input,
     Avatar,
     Badge,
     Tooltip,
     Collapse,
     Pagination,
     Switch,
+    Select,
     Waterfall,
+    Scrollbar,
+    Casader,
   } from 'star8080-ui'
 
-  import { ref } from 'vue'
+  import { reactive, ref, watch } from 'vue'
+  import testtest from './test.vue'
 
   const open_collapse = ref(false)
   const open_collapse2 = ref(true)
@@ -95,4 +136,94 @@
   const page2 = ref(1)
 
   let test = ref(true)
+  const options = reactive([
+    {
+      value: '电子产品',
+      label: '电子产品',
+      children: [
+        {
+          label: '1',
+          value: '手机',
+          children: [
+            {
+              label: '1',
+              value: '小米',
+            },
+            {
+              label: '1',
+              value: '华为',
+            },
+            {
+              label: '1',
+              value: '苹果',
+            },
+          ],
+        },
+        {
+          label: '1',
+          value: '电脑',
+          children: [
+            {
+              label: '1',
+              value: '联想',
+            },
+            {
+              label: '1',
+              value: '戴尔',
+            },
+            {
+              label: '1',
+              value: '惠普',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      label: '2',
+      value: '服装',
+      children: [
+        {
+          label: '2',
+          value: '男装',
+          children: [
+            {
+              label: '2',
+              value: '衬衫',
+            },
+            {
+              label: '2',
+              value: '裤子',
+            },
+            {
+              label: '2',
+              value: '外套',
+            },
+          ],
+        },
+        {
+          label: '2',
+          value: '女装',
+          children: [
+            {
+              label: '2',
+              value: '连衣裙',
+            },
+            {
+              label: '2',
+              value: '半身裙',
+            },
+            {
+              label: '2',
+              value: 'T恤',
+            },
+          ],
+        },
+      ],
+    },
+  ])
+  const selectedValue = ref('')
+  watch(selectedValue, () => {
+    console.log(selectedValue)
+  })
 </script>
---
+++ playground/test.vue
@@ -0,0 +1,18 @@
+<template>
+  <button>{{ count }}</button>
+  <testchild></testchild>
+</template>
+<script lang="ts">
+  import { onMounted, ref, defineComponent } from 'vue'
+  import testchild from './testchild.vue'
+
+  export default defineComponent({
+    components: { testchild },
+    setuo() {
+      let count = ref(1)
+      return {
+        count,
+      }
+    },
+  })
+</script>
